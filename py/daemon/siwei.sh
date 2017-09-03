d33afe90e6d7335ef4f3d7b109c9e0bfe37071bf
tiankun <iam@tiankun.me>
2017-09-04 00:21:05 +0800

    fix siwei daemon bug

--- py/daemon/siwei.sh
+++ py/daemon/siwei.sh
@@ -1,3 +1,3 @@
 date=`date -d 'yesterday' +%m%d`
 export PGPASSWORD=''
-psql -d mydb -U tk -c "SELECT * INTO siwei.siwei_${date} FROM traffic.spider_traffic_siwei_speed WHERE date_trunc('day', time)=date_trunc('day', now() - INTERVAL '1 day') LIMIT 10;" && psql -d mydb -U tk -c "DELETE FROM traffic.spider_traffic_siwei_speed WHERE date_trunc('day', time)=date_trunc('day', now() - INTERVAL '1 day');"&& echo complete!!
+psql -d mydb -U tk -c "SELECT * INTO siwei.siwei_${date} FROM traffic.spider_traffic_siwei_speed WHERE date_trunc('day', time)=date_trunc('day', now() - INTERVAL '1 day');" && psql -d mydb -U tk -c "DELETE FROM traffic.spider_traffic_siwei_speed WHERE date_trunc('day', time)=date_trunc('day', now() - INTERVAL '1 day');"&& echo complete!!
